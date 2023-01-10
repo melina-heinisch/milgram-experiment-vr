ec923bb7e7883fc79c5dd91c01892b4657149735
Sophia Maier <sophia.maier1@studmail.uni-wuerzburg.de>
2023-01-10 13:13:45 +0100

    slider

--- Assets/Scripts/Schieberegler.cs
+++ Assets/Scripts/Schieberegler.cs
@@ -4,30 +4,62 @@ using UnityEngine;
 
 public class Schieberegler : MonoBehaviour
 {
-    Quaternion originalRotation;
-    float originalX;
-    float originalY;
-    float offsetZ;
+    public float minZ;
+    public float maxZ;
+
+    private float lastSliderZ = -9999;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        originalRotation = transform.rotation;
-        originalX = transform.position.x;
-        originalY = transform.position.y;
+
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    private void Update()
     {
-        transform.rotation = originalRotation;
-        Vector3 position = new Vector3(originalX, originalY, transform.position.z);
-        transform.position = position;
+        if (gameObject.transform.position.z < minZ)
+        {
+            Vector3 vector3 = transform.position;
+            vector3.z = minZ;
+            transform.position = vector3;
+        }
+        else if (transform.position.z > maxZ)
+        {
+            Vector3 vector3 = transform.position;
+            vector3.z = maxZ;
+            transform.position = vector3;
+        }
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnTriggerStay(Collider other)
     {
-        Debug.Log(collision.gameObject.name);
+        Debug.Log($"Trigger with {other}");
+        if (!other.name.Contains("Hand"))
+        {
+            Debug.Log($"{other}, {other.transform.position}");
+            lastSliderZ = other.gameObject.transform.position.z;
+
+        }
+    }
+
+    //will be triggered when Controler is no longer touching slider
+    private void OnCollisionExit(Collision collision)
+    {
+        ReleaseSlider();
+    }
+
+    public void ReleaseSlider()
+    {
+        if (lastSliderZ != -9999)
+        {
+            Vector3 vector3 = transform.position;
+            vector3.z = lastSliderZ;
+            transform.position = vector3;
+            Debug.Log($"Slider released. new position {vector3}");
+
+        }
     }
 
     public void MoveSchieberegler()
@@ -37,6 +69,6 @@ public class Schieberegler : MonoBehaviour
 
     public void GetOffset(Transform transform)
     {
-        offsetZ = transform.position.z - transform.position.z;
+        //offsetZ = transform.position.z - transform.position.z;
     }
 }
