c043d36b4d688ab7c89f6c1a4e0c2132b6c01be1
Sophia Maier <sophia.maier1@studmail.uni-wuerzburg.de>
2023-01-11 13:44:07 +0100

    Working interaction

--- Assets/Scripts/PressButtonAnimation.cs
+++ Assets/Scripts/PressButtonAnimation.cs
@@ -4,23 +4,66 @@ using UnityEngine;
 
 public class PressButtonAnimation : MonoBehaviour
 {
-    private float maxHeight;
-    private float minHeight;
+    public float speed;
+    public bool buttonIsPressed;
+    public float maxY;
+    public float minY;
+
+    private bool touchedByHand;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(transform.position);
+        if (gameObject.transform.position.y < minY)
+        {
+            Vector3 vector3 = transform.position;
+            vector3.y = minY;
+            transform.position = vector3;
+        }
+        if (transform.position.y >= maxY)
+        {
+            buttonIsPressed = false;
+            Vector3 vector3 = transform.position;
+            vector3.y = maxY;
+            transform.position = vector3;
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        else
+        {
+            buttonIsPressed = true;
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
         
+        if (!touchedByHand)
+        {
+            Vector3 vector3 = transform.position;
+            vector3.y += speed * Time.deltaTime;
+            transform.position = vector3;
+        }
     }
 
-    public void PressButton()
-    {
-        Debug.Log("Button was pressed.");
-    }
+
+
+    //private void OnCollisionStay(Collision collisionInfo)
+    //{
+    //    if (collisionInfo.gameObject.name.Contains("Hand"))
+    //    {
+    //        touchedByHand = true;            
+    //    }
+    //}
+
+    //private void OnCollisionExit()
+    //{
+    //    touchedByHand = false;
+    //    Debug.Log("Not touched anymore");
+    //    gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+    //}
 }
